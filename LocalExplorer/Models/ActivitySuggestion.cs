b8080124f85ebf731c020099dead3c1ae1854662
Mac <mac@MacBook-Pro-de-Mac.local>
2024-07-02 15:22:28 +0100

    first commit

--- LocalExplorer/Models/ActivitySuggestion.cs
+++ LocalExplorer/Models/ActivitySuggestion.cs
@@ -3,26 +3,6 @@ namespace LocalExplorer.Models
 {
     public class ActivitySuggestion
     {
-        //public String suggestions { get; set; }
-        //public String city { get; set; }
-        //public String weather { get; set; }
-        //public String localtime { get; set; }
-        //public String tempC { get; set; }
-        //public String status { get; set; }
-
-
-        //public ActivitySuggestion(String suggestions, String city, String weather, String localtime, String tempC, String status)
-        //{
-        //    this.suggestions = suggestions;
-        //    this.city = city;
-        //    this.weather = weather;
-        //    this.localtime = localtime;
-        //    this.tempC = tempC;
-        //    this.status = status;
-        //}
-
-
-  
             public Suggestions Suggestions { get; set; }
             public string City { get; set; }
             public string Weather { get; set; }
